c7f8046c3f799c9595d9d5bb9c9b674d8a82ba2f
Asko Nõmm <asko@bien.ee>
2021-10-26 17:18:48 +0000

    Parse inline code, add comments, update README

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # Marky
 
-A tiny Markdown parser that spits out HTML. Currently supports only a subset of the Markdown spec.
+A tiny Markdown parser written in TypeScript that spits out HTML. Currently supports only a subset of the Markdown spec. Meant to be used with [Deno](https://deno.land).
+
+## Usage
+
+```typescript
+import marky from 'https://raw.githubusercontent.com/askonomm/marky/master/marky.ts';
+
+const html = marky('**hi there**'); // => <p><strong>hi there</strong></p>
+```
 
 ## To-do
 
 - [x] Parse bold text
 - [x] Parse italic text
 - [x] Parse paragraphs
+- [x] Parse inline code
+- [ ] Parse striked out text
+- [ ] Parse underlined text
 - [ ] Parse headings
-- [ ] Parse inline code
 - [ ] Parse code blocks
 - [ ] Parse links
 - [ ] Parse images
-- [ ] Parse striked out text
-- [ ] Parse underlined text
 - [ ] Parse quote blocks (and nested quote blocks!)
 - [ ] Parse unordered lists (and nested lists!)
 - [ ] Parse ordered lists (and nested lists!)
--- marky.ts
+++ marky.ts
@@ -1,3 +1,7 @@
+/**
+ * Parses given `content` for any bold text which sits between
+ * the double asterisk characters like `**text**`.
+ */
 function bold(content: string): string {
     const matches = content.match(/\*\*.*?\*\*/g);
     
@@ -5,6 +9,7 @@ function bold(content: string): string {
         for (const match of matches) {
             const value = match.substring(2, match.length - 2);
             const replacement = `<strong>${value}</strong>`;
+
             content = content.replace(match, replacement);
         }
     }
@@ -12,6 +17,10 @@ function bold(content: string): string {
     return content;
 }
 
+/**
+ * Parses given `content` for any italic text which sits between
+ * the underscore characters like `_text_`.
+ */
 function italic(content: string): string {
     const matches = content.match(/_.*?_/g);
 
@@ -19,6 +28,7 @@ function italic(content: string): string {
         for (const match of matches) {
             const value = match.substring(1, match.length - 1);
             const replacement = `<em>${value}</em>`;
+
             content = content.replace(match, replacement);
         }
     }
@@ -26,6 +36,32 @@ function italic(content: string): string {
     return content;
 }
 
+/**
+ * Parses given `content` for any inline code text which sits between
+ * the tick characters like ``text``.
+ */
+function inlineCode(content: string): string {
+    const matches = content.match(/\`.*\`/g);
+
+    if (matches) {
+        for (const match of matches) {
+            const value = match.substring(1, match.length - 1);
+            const replacement = `<code>${value}</code>`;
+
+            content = content.replace(match, replacement);
+        }
+    }
+    
+    return content;
+}
+
+/**
+ * Parses given `content` for double line breaks which it then
+ * turns into paragraphs. 
+ * 
+ * It currently avoids headings, but in the future there might 
+ * be more thing this needs to avoid, like blockquotes, etc.
+ */
 function paragraph(content: string): string {
     const blocks = content.split(/\n\n/);
 
@@ -43,12 +79,20 @@ function paragraph(content: string): string {
     }).join('');
 }
 
+/**
+ * Takes in raw Markdown as `content`, sends it to the heavens
+ * where it will be polished into fine bits of HTML and handed down
+ * to you with great glory.
+ */
 export default function marky(content: string): string {
-    // turns **{string}** to <strong>{string}</strong>
+    // Turns **{string}** to <strong>{string}</strong>
     content = bold(content);
 
-    // turns _{string}_ to <em>{string}</em>
+    // Turns _{string}_ to <em>{string}</em>
     content = italic(content);
+
+    // Turns `{string}` to <code>{string}</code>
+    content = inlineCode(content);
 
     // turns two line breaks into paragraphs
     content = paragraph(content);
