1d016f360eb109f1865c1584369ca62fab017daa
Asko Nõmm <asko@bien.ee>
2021-10-27 02:36:57 +0000

    More consistent test naming

--- marky_test.ts
+++ marky_test.ts
@@ -2,35 +2,35 @@ import { marky } from "./marky.ts";
 import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
 
 // Test bold text parsing and conversion
-Deno.test("Test bold text parsing and conversion", () => {
+Deno.test("bold text parsing and conversion", () => {
   const testString = `Hello **Mr. Bond**.`;
   const expectedResult = `<p>Hello <strong>Mr. Bond</strong>.</p>`;
   assertEquals(expectedResult, marky(testString));
 });
 
 // Test italic text parsing and conversion
-Deno.test("Test italic text parsing and conversion", () => {
+Deno.test("italic text parsing and conversion", () => {
   const testString = `Hello _Mr. Bond_.`;
   const expectedResult = `<p>Hello <em>Mr. Bond</em>.</p>`;
   assertEquals(expectedResult, marky(testString));
 });
 
 // Test inline code text parsing and conversion
-Deno.test("Test inline code text parsing and conversion", () => {
+Deno.test("inline code text parsing and conversion", () => {
   const testString = `Hello \`Mr. Bond\`.`;
   const expectedResult = `<p>Hello <code>Mr. Bond</code>.</p>`;
   assertEquals(expectedResult, marky(testString));
 });
 
 // Test strikethrough text parsing and conversion
-Deno.test("Test strikethrough text parsing and conversion", () => {
+Deno.test("strikethrough text parsing and conversion", () => {
   const testString = `Hello ~~Mr. Bond\~~.`;
   const expectedResult = `<p>Hello <del>Mr. Bond</del>.</p>`;
   assertEquals(expectedResult, marky(testString));
 });
 
 // Test block parsing and conversion
-Deno.test("Test block parsing and conversion", () => {
+Deno.test("block parsing and conversion", () => {
   const testString = `
 # The Villain
 
